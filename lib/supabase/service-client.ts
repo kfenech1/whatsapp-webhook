53b042c1db4b958e91ce445b6c9c5a61a406c658
keithfenech <keith@accelbyte.net>
2023-10-22 17:55:05 +0300

    set persist session to false

--- lib/supabase/service-client.ts
+++ lib/supabase/service-client.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js"
 
 function createServiceClient() {
-    return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE)
+    return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE, {
+        auth: {
+          persistSession: false
+        }
+      })
 }
 
 export {
