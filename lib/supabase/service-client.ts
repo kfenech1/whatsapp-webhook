11bac0a1a5614a9c23419bab8f3fd25e3dc11335
keithfenech <keith@accelbyte.net>
2023-10-22 18:16:48 +0300

    set persist session to true

--- lib/supabase/service-client.ts
+++ lib/supabase/service-client.ts
@@ -2,9 +2,7 @@ import { createClient } from "@supabase/supabase-js"
 
 function createServiceClient() {
     return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE, {
-        auth: {
-          persistSession: false
-        }
+        auth: { persistSession: true, storage: sessionStorage }
       })
 }
 
